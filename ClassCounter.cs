438d76fe0354e76811d6070a4d63eaba0ad7810a
KRIBELL <127005164+KRIBELL@users.noreply.github.com>
2023-04-01 21:03:05 +0700

    Add files via upload

---
+++ ClassCounter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace  HOMEWORK_4
+{
+    class ClassCounter
+    {
+        public delegate void SomeMethod1();
+        public void Count()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                if (i == 77)
+                {
+                    Count();
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HOMEWORK_4;
+
+class Program
+{
+    static void Main(string[] args, ClassCounter counter)
+    {
+        ClassCounter Counter = new ClassCounter();
+        Handler1 Handler1 = new Handler1();
+        Handler2 Handler2 = new Handler2();
+
+        Counter.Count += Handler1.Message;
+        Counter.Count += Handler2.Message;
+        Counter.Count();
+    }
+}
